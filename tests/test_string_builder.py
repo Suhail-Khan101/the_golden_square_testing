ed16a20c138c799e900a6eb9e44f9356606a6d52
Suhail Khane <sqk777@gmail.com>
2025-06-12 14:37:12 +0100

    updated changes

--- tests/test_string_builder.py
+++ tests/test_string_builder.py
@@ -1,1 +1,22 @@
-from lib.StringBuilder import *
+from lib.string_builder import *
+
+def test_initially_is_output_is_an_empty_string():
+    string_builder = StringBuilder()
+    assert string_builder.output() == ""
+
+def test_adding_a_string_outputs_that_string():
+    string_builder = StringBuilder()
+    string_builder.add("hello")
+    assert string_builder.output() == "hello"
+
+def test_adding_a_string_sets_size_to_that_string_size():
+    string_builder = StringBuilder()
+    string_builder.add("hello")
+    assert string_builder.size() == 5
+
+def test_adding_multiple_strings_outputs_concatenated_strings():
+    string_builder = StringBuilder()
+    string_builder.add("hello")
+    string_builder.add(" ")
+    string_builder.add("world")
+    assert string_builder.size() == 11
